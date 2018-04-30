38801937e85d5498018ec71933d2a0076ec15e7a
Jonscot <sentryrook@gmail.com>
2018-04-29 20:47:08 -0400

    refactor test

--- test/sla_manager.js
+++ test/sla_manager.js
@@ -1,11 +1,11 @@
 var SlaManager = artifacts.require("./SlaManager.sol");
 
 contract('slaManager', function(accounts) {
+  var sla_manager;
   it("should assert true", function() {
-    var sla_manager;
     return SlaManager.deployed().then(function(instance){
       sla_manager = instance;
-      sla_manager.registerName("SLA001-good",accounts[5],1);
+      return sla_manager.registerName("SLA001-good",accounts[5],1);
     }).then(function(result){
       console.log(result);
       return sla_manager.getContractInfo.call("SLA001-good");
@@ -17,7 +17,7 @@ contract('slaManager', function(accounts) {
       console.log("version: ",version);
       assert.equal(address, accounts[5], "address is same as address 5");
       assert.equal(version, 1, "version should be 1");
-      sla_manager.registerName("SLA001-good",accounts[6],2);
+      return sla_manager.registerName("SLA001-good",accounts[6],2);
     }).then(function(result){
       console.log(result);
       return sla_manager.getContractInfo.call("SLA001-good");
@@ -29,6 +29,31 @@ contract('slaManager', function(accounts) {
       console.log("version: ",version);
       assert.equal(address, accounts[6], "address is same as address 6");
       assert.equal(version, 2, "version should be 2");
+    });
+  });
+  it("should assert true", function() {
+      return sla_manager.registerName("SLA002-good",accounts[5],1).then(function(instance){
+    }).then(function(result){
+      console.log(result);
+      return sla_manager.getContractInfo.call("SLA002-good");
+    }).then(function(result){
+      console.log("getContractInfo: ", result);
+      var address = result[0];
+      var version = result[1].toNumber();
+      console.log("address: ",address);
+      console.log("version: ",version);
+      assert.equal(address, accounts[5], "address is same as address 5");
+      assert.equal(version, 1, "version should be 1");
+      sla_manager.removeContract("SLA002-good");
+      return sla_manager.getContractInfo.call("SLA002-good");
+    }).then(function(result){
+      console.log("getContractInfo: ", result);
+      var address = result[0];
+      var version = result[1].toNumber();
+      console.log("address: ",address);
+      console.log("version: ",version);
+      assert.equal(address, 0x0, "address is empty");
+      assert.equal(version, 0, "version should be 0");
     });
   });
 });
